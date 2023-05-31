cf06e597d75ac2019d96b2a3c54bba5c7ba79586
Nethpali_Jayaweera <nethpalijayaweera@gmail.com>
2023-05-31 09:00:18 +0530

    BoFactory Commit

---
+++ Layered-Project-main/Layered_Architecture/src/Bo1/BoFactory.java
@@ -0,0 +1,32 @@
+package Bo1;
+
+import bo.Custom.CustomerBoImpl;
+import bo.Custom.ItemBoImpl;
+import bo.Custom.PlaceOrderBoImpl;
+
+public class BoFactory {
+    private static BoFactory boFactory;
+    private BoFactory(){
+    }
+    public static BoFactory getBoFactory(){
+        return (boFactory==null)? boFactory=new BoFactory() : boFactory;
+    }
+
+    public enum BOTypes{
+        CUSTOMER,ITEM,PO
+    }
+
+    //Object creation logic for BO objects
+    public SuperBo getBO(BOTypes types){
+        switch (types){
+            case CUSTOMER:
+                return new CustomerBoImpl();
+            case ITEM:
+                return new ItemBoImpl();
+            case PO:
+                return new PlaceOrderBoImpl();
+            default:
+                return null;
+        }
+    }
+}
